196f16583bed0bec49e631c92df8d0bf3c5648ea
TheDoctor <m@emdienn.com>
2021-12-20 21:44:15 +1100

    Add server component

---
+++ src/lib/server/index.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import { init } from './index'
+  import { createEventDispatcher, onMount } from 'svelte'
+
+  import type { JanusJS } from 'janus-gateway-ts'
+
+  const dispatch = createEventDispatcher()
+
+  export let server: JanusJS.ConstructorOptions['server']
+  export let apisecret: JanusJS.ConstructorOptions['apisecret'] = undefined
+  export let withCredentials: JanusJS.ConstructorOptions['withCredentials'] = undefined
+  export let token: JanusJS.ConstructorOptions['token'] = undefined
+  export let iceServers: JanusJS.ConstructorOptions['iceServers'] = undefined
+  export let ipv6: JanusJS.ConstructorOptions['ipv6'] = undefined
+  export let maxPollEvents: JanusJS.ConstructorOptions['max_poll_events'] = undefined
+  export let debug: JanusJS.InitOptions['debug'] = undefined
+  export let dependencies: JanusJS.InitOptions['dependencies'] = undefined
+
+  let janus: JanusJS.Janus
+  let error: Error
+
+  // once the component mounts, attempt a connection to Janus with the configuration provided
+  // on success, the slot will render and sub-components can mount
+  onMount(async () => {
+    try {
+      janus = await init({ server, debug, dependencies, apisecret, iceServers, ipv6, maxPollEvents, token, withCredentials })
+    } catch (e) {
+      // render our slot and dispatch our message
+      error = e
+      dispatch('error', e)
+    }
+  })
+
+</script>
+
+{#if janus}
+  <slot {janus} />
+{:else if error}
+  <slot name="error" {error} />
+{/if}
---
+++ src/lib/server/index.ts
@@ -0,0 +1,41 @@
+import Janus from 'janus-gateway-ts'
+import adapter from 'webrtc-adapter'
+
+import type { JanusJS } from 'janus-gateway-ts'
+
+// incorporate the webrtc adapter into our dependencies by default
+const deps = (dependencies:JanusJS.Dependencies) => Janus.useDefaultDependencies({ adapter, ...dependencies })
+
+type InitOptions = {
+  server: JanusJS.ConstructorOptions['server']
+  apisecret?: JanusJS.ConstructorOptions['apisecret']
+  withCredentials?: JanusJS.ConstructorOptions['withCredentials']
+  token?: JanusJS.ConstructorOptions['token']
+  iceServers?: JanusJS.ConstructorOptions['iceServers']
+  ipv6?: JanusJS.ConstructorOptions['ipv6']
+  maxPollEvents?: JanusJS.ConstructorOptions['max_poll_events']
+
+  debug?: JanusJS.InitOptions['debug']
+  dependencies?: JanusJS.InitOptions['dependencies']
+}
+
+type Init = (options:InitOptions) => Promise<Janus>
+
+/**
+ * Promise to initialise a new connection to Janus
+ */
+export const init:Init = ({ debug, server, dependencies }) => new Promise(
+  (resolve, reject) => Janus.init({ debug, dependencies: deps(dependencies), callback: () => {
+    const janus = new Janus({
+      server,
+      destroyed: () => Janus.log('Janus be destroy'),
+      error: e => reject(e),
+      success: () => resolve(janus)
+    })
+  }})
+)
+
+
+// components
+
+export { default as Server } from './index.svelte'

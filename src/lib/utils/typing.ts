de4cdc32278a118b1fc177535f4cc909ddfd2993
TheDoctor <m@emdienn.com>
2021-12-21 17:06:54 +1100

    Add XOR types

---
+++ src/lib/utils/typing.ts
@@ -0,0 +1,16 @@
+/**
+ * XOR in typescript. Amazing.
+ *
+ * @credit tjjfvi <https://stackoverflow.com/users/11860232/tjjfvi>
+ * @see https://stackoverflow.com/questions/42123407/does-typescript-support-mutually-exclusive-types#comment123255834_53229567
+ * @demo https://tsplay.dev/wgLpBN
+ */
+
+type UnionKeys<T> = T extends T ? keyof T : never
+
+// Improve intellisense
+type Expand<T> = T extends T ? { [K in keyof T]: T[K] } : never
+
+export type OneOf<T extends {}[]> = {
+  [K in keyof T]: Expand<T[K] & Partial<Record<Exclude<UnionKeys<T[number]>, keyof T[K]>, never>>>
+}[number]

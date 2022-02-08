632f7a505e10e429340590e1dd2430b7f4cb6cc2
TheDoctor <m@emdienn.com>
2022-02-08 16:11:21 +1100

    Prepare for packaging

---
+++ svelte.config.js
@@ -0,0 +1,30 @@
+import adapter from '@sveltejs/adapter-auto'
+import path from 'path'
+import preprocess from 'svelte-preprocess'
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: adapter(),
+
+		package: {
+			exports: file => file === 'index.ts'
+		},
+		vite: {
+			resolve: {
+				alias: {
+					'janus-svelte': path.resolve('src/lib')
+				}
+			}
+		},
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
+	}
+};
+
+export default config;

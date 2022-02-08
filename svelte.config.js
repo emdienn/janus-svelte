58ff5b82647ef345e8cc2720518096d706f71eef
TheDoctor <m@emdienn.com>
2022-02-08 21:50:43 +1100

    Format root configs

--- svelte.config.js
+++ svelte.config.js
@@ -4,27 +4,27 @@ import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter(),
+  kit: {
+    adapter: adapter(),
 
-		package: {
-			exports: file => file === 'index.ts'
-		},
-		vite: {
-			resolve: {
-				alias: {
-					'janus-svelte': path.resolve('src/lib')
-				}
-			}
-		},
+    package: {
+      exports: file => file === 'index.ts',
+    },
+    vite: {
+      resolve: {
+        alias: {
+          'janus-svelte': path.resolve('src/lib'),
+        },
+      },
+    },
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
-};
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+  },
+}
 
-export default config;
+export default config
